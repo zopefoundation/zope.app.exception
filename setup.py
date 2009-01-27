d53ba51d3f8be5f00552f98efc0b985e4ae1421e
Michael Howitz <mh@gocept.com>
2009-01-27 08:23:14 +0000

    preparing release

--- setup.py
+++ setup.py
@@ -24,7 +24,7 @@ def read(*rnames):
 setup(name='zope.app.exception',
       version = '3.5.0',
       author='Zope Corporation and Contributors',
-      author_email='zope3-dev@zope.org',
+      author_email='zope-dev@zope.org',
       description='Zope 3 exception views',
       long_description=(
           read('README.txt')
@@ -42,7 +42,7 @@ setup(name='zope.app.exception',
           'Operating System :: OS Independent',
           'Topic :: Internet :: WWW/HTTP',
           'Framework :: Zope3'],
-      url='http://cheeseshop.python.org/pypi/zope.app.exception',
+      url='http://pypi.python.org/pypi/zope.app.exception',
       license='ZPL 2.1',
       packages=find_packages('src'),
       package_dir = {'': 'src'},
